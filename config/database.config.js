4e8a1a36f6137f6bad7408f23ec4da155b146379
Anubhav Dhuliya <dhuliya.a@gmail.com>
2019-12-31 14:59:56 +0530

    changes made to deploy to heroku

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://<dbuser>:<dbpassword>@ds143039.mlab.com:43039/heroku_j927d5dp://localhost:27017/flobiz'
+    url: 'mongodb://<heroku_j927d5dp>:<instagram01>@ds143039.mlab.com:43039/heroku_j927d5dp://localhost:27017/flobiz'
 }
